c9ca1fb626da4c27899f3a66c6eb34c128527400
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-01-21 21:20:20 +0100

    add styling to PrimaryButton component

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -2,12 +2,41 @@ import { Pressable, View, Text, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={asd}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
