52dfabc16ebaa6ca430b86210aecc7e2b7925d12
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-01-22 15:34:32 +0100

    create Title component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,30 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 function GameScreen() {
-  return <Text>Game screen</Text>;
+  return (
+    <View style={styles.screen}>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      <View></View>
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#ddb52f",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: "#ddb52f",
+    padding: 12,
+  },
+});
